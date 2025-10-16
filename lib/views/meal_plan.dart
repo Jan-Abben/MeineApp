e6734a72cdc3d44861a359b9d93e88e65074d806
Jan Abbenhaus <jan@abbenhaus.de>
2025-10-16 14:13:06 +0200

    added meal plan file

---
+++ lib/views/meal_plan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MealPlan extends StatefulWidget {
+  const MealPlan({super.key});
+
+  @override
+  State<MealPlan> createState() => _MealPlanState();
+}
+
+class _MealPlanState extends State<MealPlan> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
