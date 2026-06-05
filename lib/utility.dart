9bde835b0a9393f95f923bf3ce89811ef378c0b1
Jan Abbenhaus <jan@abbenhaus.de>
2026-06-05 11:29:04 +0200

    Added functions to call database and displayed database content in food_database

---
+++ lib/utility.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+//Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+
+//Hilfsklassen und Methoden
+class PageItem {
+  final String title;
+  final Widget page;
+  final IconData icon;
+
+  const PageItem({required this.title, required this.page, required this.icon});
+}
+
+class FoodItem {
+  final String name;
+  final int calories;
+  final int protein;
+
+  FoodItem({required this.name, required this.calories, required this.protein});
+}
+
+class RecipeItem {
+  final String name;
+  final Map<String, int> ingredients; // Map von Lebensmittelname zu Menge
+  final int calories;
+  final int protein;
+
+  RecipeItem({required this.name, required this.ingredients, required this.calories, required this.protein});
+}
+
+
+
+class DatabaseService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<void> addFoodItem(FoodItem foodItem) async {
+    await _db.collection('Lebensmittel').doc(foodItem.name).set({
+      'calories': foodItem.calories,
+      'protein': foodItem.protein,
+    });
+    
+  }
+
+  Future<FoodItem> getFoodItem(String name) async {
+    final documentSnapshot = await _db.collection('Lebensmittel').doc(name).get();
+    if (documentSnapshot.exists) {
+      final data = documentSnapshot.data()!;
+      return FoodItem(
+        name: name,
+        calories: data['calories'],
+        protein: data['protein'],
+      );
+    } else {
+      throw Exception('Food item not found');
+    }
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getAllFoodItems() {
+    return FirebaseFirestore.instance
+      .collection('Lebensmittel')
+      .snapshots();
+  }
+
+
+  Future<void> addRecipeItem(RecipeItem recipeItem) async {
+    await _db.collection('Rezepte').doc(recipeItem.name).set({
+      'ingredients': recipeItem.ingredients,
+      'calories': recipeItem.calories,
+      'protein': recipeItem.protein,
+    });
+    
+  }
+
+  Future<RecipeItem> getRecipeItem(String name) async {
+    final documentSnapshot = await _db.collection('Rezepte').doc(name).get();
+    if (documentSnapshot.exists) {
+      final data = documentSnapshot.data()!;
+      return RecipeItem(
+        name: name,
+        ingredients: data['ingredients'],
+        calories: data['calories'],
+        protein: data['protein'],
+      );
+    } else {
+      throw Exception('Recipe item not found');
+    }
+  }
+}
--- lib/views/food_database.dart
+++ lib/views/food_database.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utility.dart';
+
 class FoodDatabase extends StatefulWidget {
   const FoodDatabase({super.key});
 
@@ -10,6 +12,47 @@ class FoodDatabase extends StatefulWidget {
 class _FoodDatabaseState extends State<FoodDatabase> {
   @override
   Widget build(BuildContext context) {
-    return Text("Lebensmittel-Datenbank");
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: StreamBuilder(
+            stream: DatabaseService().getAllFoodItems(),
+            builder: (context, snapshot) {
+              
+              if (snapshot.hasError) {
+                return Text('Fehler');
+              }
+          
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+          
+              final docs = snapshot.data!.docs;
+          
+              return ListView.builder(
+                itemCount: docs.length,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text(docs[index].id));
+                },
+              );
+            },
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: SizedBox(
+            width: 75,
+            height: 75,
+            child: FloatingActionButton(
+              onPressed: () {},
+              tooltip: 'Neu hinzufügen',
+              child: const Icon(Icons.add, size: 40),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
